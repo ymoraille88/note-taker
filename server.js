085a10080cb58c2e08145118c809e5ab8c61a0d9
Yves-Henry Moraille <yves-henrymoraille@Yves-Henrys-Air.lan>
2023-01-30 15:12:48 -0500

    pushing to heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const uuid = require('./helpers/uuid')
 
 
 const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
